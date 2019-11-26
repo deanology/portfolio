187eef064d5477185c8b1821e53c84db5adc8046
Idam Okechukwu Samuel <okechukwuidam516@gmail.com>
2019-11-26 15:03:08 -0800

    Full changes made to the whole portfolio

--- script/main.js
+++ script/main.js
@@ -17,3 +17,23 @@ $(function(){
 		time: 3000
 	});
 });
+
+
+$(function(){
+	$(".filter-button").click(function(){ //click on the function
+		var value = $(this).attr('data-filter');
+
+		if(value == "all"){
+			$('.filter').show('1000');
+		}else{
+			$(".filter").not('.'+ value).hide('3000');
+			$(".filter").filter('.'+value).show('3000');
+
+		}
+	});
+
+	if($(".filter-button").removeClass("active")){
+		$(this).removeClass("active");
+	}
+	$(this).addClass("active");
+});
